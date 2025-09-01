dc694d72ab6181abd542362e048b7d3188fc67fa
Abhishek-5-2 <pundir0224@gmail.com>
2025-09-01 19:02:27 +0530

    todo list app

---
+++ js/script.js
@@ -0,0 +1,247 @@
+const taskContainer = JSON.parse(localStorage.getItem("tasks")) || [];
+const addBtn = document.querySelector("#add-task1-1");
+const deleteAllTaskBtn=document.querySelector("#all-task-del");
+// console.log(deleteAllTaskBtn);
+addBtn.addEventListener("click", addTask);
+const taskInputBox = document.querySelector("#task1-1");
+console.log(taskInputBox);
+taskInputBox.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
+
+const container = document.querySelector(".all-task-container");
+console.log(container);
+
+        function renderUI() {
+          if (taskContainer.length === 0) {
+            container.classList.add("fade-out");
+            setTimeout(() => {
+              container.innerHTML = `
+                 <p>No Tasks Yet!</p>
+                 <button id="click-here">Click to add Tasks...</button>
+               `;
+              container.classList.add("place-center");
+
+              container.querySelector("#click-here").onclick= () => { taskInputBox.focus();  };
+               
+            
+
+              container.classList.remove("fade-out");
+              container.classList.add("fade-in");
+
+              // Remove fade-in after animation ends
+              setTimeout(() => container.classList.remove("fade-in"), 300);
+            }, 200);
+
+            saveTasks();
+            return;
+          }
+
+          //container.classList.remove("place-center");
+
+          let containerHtml = "";
+          taskContainer.forEach((task, i) => {
+            const inptId = `id${i}`;
+            const html = `<div class="task-container">
+           <input id="${inptId}" class="check-box" type="checkbox" ${
+              task.checked ? "checked" : ""
+            }>
+           <p class="text-contant">${task.text}</p>
+           <button class="delete-btn"><img class="del-img" src="../assets/delete.png"></button>
+          </div>`;
+            containerHtml += html;
+            // console.log(containerHtml);
+          });
+          container.innerHTML = containerHtml;
+          // console.log(container.querySelector(inptId));
+          //   const chkBoxInput = container.querySelector(inptId);
+          //   chkBoxInput.checked = task.checked;
+          //   chkBoxInput.addEventListener("change", () => {
+          //     taskContainer[i].checked = chkBoxInput.checked;
+          //     saveTasks();
+          //   });
+
+          // div.addEventListener("click", (event) => {
+          //   if (
+          //     event.target !== chkBoxInput &&
+          //     event.target.tagName !== "BUTTON" &&
+          //     event.target.tagName !== "IMG"
+          //   ) {
+          //     chkBoxInput.checked = !chkBoxInput.checked;
+          //     taskContainer[i].checked = chkBoxInput.checked;
+          //     saveTasks();
+          //   }
+          // });
+
+          // const dltBtn = document.createElement("button");
+          // dltBtn.innerHTML = `<img class="del-img" src="/assets/delete.png">`;
+          // dltBtn.className = "delete-btn";
+
+          // dltBtn.addEventListener("click", (e) => {
+          //   e.stopPropagation();
+          //   div.classList.add("fade-out");
+          //   setTimeout(() => {
+          //     taskContainer.splice(i, 1);
+          //     renderUI();
+          //   }, 300);
+          // });
+
+          // setTimeout(() => div.classList.add("show"), 10);
+
+          saveTasks();
+        }
+renderUI();
+
+container.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-btn")) {
+    console.log("i m deleted");
+
+    const index = [...container.querySelectorAll(".delete-btn")].indexOf(
+      event.target
+    );
+    console.log(index);
+    taskContainer.splice(index, 1);
+    console.log(taskContainer);
+    renderUI();
+  } else if (event.target.classList.contains("del-img")) {
+    console.log("hello");
+    const index = [...container.querySelectorAll(".del-img")].indexOf(
+      event.target
+    );
+    console.log(index);
+    taskContainer.splice(index, 1);
+    console.log(taskContainer);
+    renderUI();
+  }
+
+  if (event.target.classList.contains("check-box")) {
+    const index = [...container.querySelectorAll(".check-box")].indexOf(event.target);
+     console.log(index);
+    taskContainer[index].checked = event.target.checked;
+     console.log(taskContainer[index].checked, taskContainer[index]);
+    saveTasks();
+  }
+});
+
+function taskDeleteBtn(event) {
+  if (event.target.classList.contains("delete-btn")) {
+    console.log("i m deleted");
+
+    const index = [...container.querySelectorAll(".delete-btn")].indexOf(
+      event.target
+    );
+    console.log(index);
+    taskContainer.splice(index, 1);
+    console.log(taskContainer);
+    renderUI();
+  } else if (event.target.classList.contains("del-img")) {
+    console.log("hello");
+    const index = [...container.querySelectorAll(".del-img")].indexOf(
+      event.target
+    );
+    console.log(index);
+    taskContainer.splice(index, 1);
+    console.log(taskContainer);
+    renderUI();
+  }
+}
+
+// const delBtn=container.querySelectorAll(".delete-btn");
+// console.log(delBtn);
+// console.log(delBtn.length);
+
+// Helper to save to localStorage
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(taskContainer));
+}
+
+function addTask() {
+  const taskInBoxValue = taskInputBox.value.trim();
+  if (taskInBoxValue === "") {
+    document.querySelector("#task1-1").focus();
+    // console.log(document.querySelector("#task1-1"));
+    return;
+  } else if (taskInBoxValue !== "") {
+    taskContainer.push({ text: taskInBoxValue, checked: false });
+    taskInputBox.value = "";
+    document.getElementById("to-do-tasks-1-1").classList.remove("place-center");
+    // console.log(taskContainer[0].text);
+    saveTasks();
+    console.log(taskContainer);
+
+    renderUI();
+
+    // Scroll to the latest task
+    const lastTask =
+      document.querySelector("#to-do-tasks-1-1").lastElementChild;
+    if (lastTask) lastTask.scrollIntoView({ behavior: "smooth", block: "end" });
+  }
+
+  //or
+  // const container = document.querySelector(".main-container-div");
+  // container.scrollTo({ top: container.scrollHeight, behavior: "smooth" });
+}
+
+function deleteAllTask() {
+  taskContainer.splice(0, taskContainer.length);
+  console.log(taskContainer);
+  renderUI();
+}
+// delete all task button
+deleteAllTaskBtn.addEventListener("click", () => {
+  if (taskContainer.length === 0) {
+    confirmationPopUp("No tasks to delete!", false);
+  } else {
+    confirmationPopUp(
+      "Are you sure you want to delete all tasks?",
+      deleteAllTask
+    );
+  }
+});
+
+function confirmationPopUp(message, confirmAction) {
+  const overlay = document.createElement("div");
+  overlay.className = "overlay";
+  const popup = document.createElement("div");
+  popup.className = "popup";
+
+  // Message text
+  const msg = document.createElement("p");
+  msg.textContent = message;
+  msg.className = "para-style";
+  popup.appendChild(msg);
+  const confirmBtn = document.createElement("button");
+  confirmBtn.textContent = "Confirm";
+  confirmBtn.id = "confirm-del";
+  if (taskContainer.length) {
+    confirmBtn.addEventListener("click", () => {
+      confirmAction();
+      popup.remove();
+      overlay.remove();
+    });
+  } else {
+    confirmBtn.addEventListener("click", () => {
+      popup.remove();
+      overlay.remove();
+    });
+  }
+
+  popup.appendChild(confirmBtn);
+
+  const cancelBtn = document.createElement("button");
+  cancelBtn.id = "cancel-btn";
+  cancelBtn.textContent = "Cancel";
+  cancelBtn.addEventListener("click", () => {
+    popup.remove();
+    overlay.remove();
+  });
+  popup.appendChild(cancelBtn);
+
+  // Append popup to page
+  document.body.appendChild(overlay);
+  document.body.appendChild(popup);
+}
+
+// renderTasks();
